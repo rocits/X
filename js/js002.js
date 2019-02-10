c48ac34869b8ad1dedcd0befbe71a416473668e6
Rory O Connor <rocits@users.noreply.github.com>
2019-02-10 02:33:27 +0000

    Create js002.js

---
+++ js/js002.js
@@ -0,0 +1,10 @@
+// https://rocits.github.io/X/js/js002.js
+
+links=document.getElementsByTagName(%22a%22);
+%20for%20(var%20l%20=%200;%20l%20<%20links.length;%20l++){%20var%20alllinks;
+%20alllinks%20+=%20links[l].outerHTML;
+links[l].href;
+alllinks%20+=%20%22<br><br>%20%22%20;
+alllinks%20+=%20links[l].title+%22%C2%A0:%C2%A0%22;
+alllinks%20+=%20%22%22%20};
+window.open(%22data:text/html,%22+encodeURI(alllinks),%22_blank%22,%22toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400%22);document.write(%22URL,Title<br>%22+alllinks);
