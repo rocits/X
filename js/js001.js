c6fbc18eb8bbec191ca4be2c8ec937c7c8688c1b
Rory O Connor <rocits@users.noreply.github.com>
2019-02-10 08:40:25 +0000

    Update js001.js

--- js/js001.js
+++ js/js001.js
@@ -1,5 +1,8 @@
 // https://rocits.github.io/X/js/js001.js
 
+var images = document.getElementsByTagName('*');var l = images.length;for (var i = 0; i < l; i++) {    images[i].removeAttribute("class" && "jsname" && "jslog" && "aria-label" && "autofocus" && "target" && "jsaction" && "jscontroller" );
+images[i].removeAttribute("data-pid" && "draggable" && "id" && "aria-expanded" && "aria-hidden" && "title" && "data-pid" && "aria-expanded" && "role" && "tabindex");                                                                                                                         images[i].removeAttribute("id" && "style" && "aria-label" && "expanded");}
+/*
 var elements = document.getElementsByTagName('*');
 var l = elements.length;
 for (var i = 0; i < l; i++){
@@ -8,4 +11,4 @@ var attnames += elements[i].attributes;}}
 console.log(attnames);
 
 Array.prototype.slice.call(attnames).forEach(function(item) { 
-      elements.removeAttribute(item.name);}
+      elements.removeAttribute(item.name);} */
