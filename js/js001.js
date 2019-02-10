8ce0230d13315bd511b529670530679b9ae5ad63
Rory O Connor <rocits@users.noreply.github.com>
2019-02-10 02:10:46 +0000

    Update js001.js

--- js/js001.js
+++ js/js001.js
@@ -2,4 +2,4 @@ var elements = document.getElementsByTagName('*');
 var l = elements.length;
 for (var i = 0; i < l; i++)
 {elements[i].removeAttribute("class");
-console.log = i;}}}());
+console.log = i;}
