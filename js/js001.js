29a6eb9778e9e583a766c183bc602accff1c886a
Rory O Connor <rocits@users.noreply.github.com>
2019-02-18 20:12:00 +0000

    Update js001.js

--- js/js001.js
+++ js/js001.js
@@ -1,4 +1,4 @@
-javascript:(function(){window.open('https://rocits.github.io/X/js/index.html','_blank','toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400');;})()
+window.open('https://rocits.github.io/X/js/index.html','_blank','toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400');
 
 //var images = document.getElementsByTagName('*');var l = images.length;for (var i = 0; i < l; i++) { images[i].removeAttribute("class" && "jsname" && "jslog" && "aria-label" && "autofocus" && "target" && "jsaction" && "jscontroller" );images[i].removeAttribute("data-pid" && "draggable" && "id" && "aria-expanded" && "aria-hidden" && "title" && "data-pid" && "aria-expanded" && "role" && "tabindex");images[i].removeAttribute("id" && "style" && "aria-label" && "expanded");}
 
